5c0bd5e4f7485b7565a069792b3d562a673369ec
chasepilon <chasepilon@gmail.com>
2020-12-28 12:55:33 -0500

    RubyProject
    Ruby's 2D Adventure project

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2D.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController enemy = other.collider.GetComponent<EnemyController>();
+        if(enemy != null)
+        {
+            enemy.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
